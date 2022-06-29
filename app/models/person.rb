40f551610f8a1d7898d9b600b1afc1fda9d9c47b
rana haseeb <alix@yahoo.com>
2022-06-29 13:56:26 +0500

    translation_active_model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,15 +1,3 @@
 class Person
-  include ActiveModel::Serializers::JSON
-
-  attr_accessor :name
-
-  def attributes=(hash)
-    hash.each do |key, value|
-      send("#{key}=", value)
-    end
-  end
-
-  def attributes
-    {'name' => nil}
-  end
+  extend ActiveModel::Translation
 end
