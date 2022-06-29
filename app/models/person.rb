afd757fe28c92fe06930d5a21d7cfee36bd681b7
rana haseeb <alix@yahoo.com>
2022-06-29 13:12:10 +0500

    naming_active_model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,3 @@
 class Person
-  include ActiveModel::Validations
-
-  attr_accessor :name, :email, :token
-
-  validates :name, presence: true
-  validates_format_of :email, with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
-  validates! :token, presence: true
+  extend ActiveModel::Naming
 end
